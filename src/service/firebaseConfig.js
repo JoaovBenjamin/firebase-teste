31db8cc48b0ca8b6d65f7c077bf27930bfac0719
Joaovvb <rm98938@fiap.com.br>
2024-12-03 23:56:09 -0300

    register e login

---
+++ src/service/firebaseConfig.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBDkPt9ncRkaH0AKbH_BnSUnziyngYzED4",
+  authDomain: "app-teste-7c54e.firebaseapp.com",
+  projectId: "app-teste-7c54e",
+  storageBucket: "app-teste-7c54e.firebasestorage.app",
+  messagingSenderId: "978751805524",
+  appId: "1:978751805524:web:81955fac6b9dfdb49875c1",
+  measurementId: "G-MT32MPCQFZ"
+};
+
